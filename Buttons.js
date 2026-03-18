050285b0fcf86e2f5a18e946dcab1d6d7ffa4d74
ATN1307 <arjun.t.n0713@gmail.com>
2026-03-18 13:35:20 -0500

    Implement button click handlers in Buttons.js
    Let's push something

---
+++ Buttons.js
@@ -0,0 +1,45 @@
+
+const whatButton = document.getElementById("what-button");
+const noiseButton = document.getElementById("noise-button");
+const interviewButton = document.getElementById("interview-button");
+// const guestbookPGButton = document.getElementById("guestbook-pg-button");
+// const guestbookMSMSButton = document.getElementById("guestbook-msms-button");
+// const guestbookBHSButton = document.getElementById("guestbook-bhs-button");
+
+const gbButton = document.getElementById("gb-button");
+// file archive
+const faPGButton = document.getElementById("fa-pg-button");
+const faMSMSButton = document.getElementById("fa-msms-button");
+const faBHSButton = document.getElementById("fa-bhs-button");
+
+// back buttons
+const backnoise = document.getElementById("back-noise");
+const backpicture = document.getElementById("back-msms");
+const backinterview = document.getElementById("back-interview");
+
+
+whatButton.onclick = function () {
+  window.open("About_Bordeaux.html");
+} 
+noiseButton.onclick = function () {
+  window.open("Noise.html");
+}
+interviewButton.onclick = function () {
+  window.open("Interview.html");
+}
+
+// back clicks
+backnoise.onclick = function () {
+  animPut(noiseDiv,"Downloads/Laptop.mp3", commonImagePath + "pinksky.jpeg", homescreen);
+}
+
+// extras
+picButton.onclick = function () {
+  window.open("Pictures.html");
+}
+// What button
+whatButton.onclick = function () {
+  window.open("About_Bordeaux.html");
+}
+
+
